8ad8ca1b241ead4aabdf935ff7ccdc9dda39c9ed
PiriyaV <piriya_v@gpo.or.th>
2020-12-20 15:31:36 +0700

    formatting code

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -17,29 +17,36 @@ describe('A Validator', function () {
                 nonDivisibleValidationRule(5, 'error.five'),
             ]);
         });
+
         it('for valid numbers, will return no errors', function () {
             expect(validator(7)).to.be.empty;
         });
+
         context('for not strictly positive numbers:', function () {
             it('like 0, will include error.nonpositive', function () {
                 expect(validator(0)).to.include('error.nonpositive');
             });
+
             it('like -2, will include error.nonpositive', function () {
                 expect(validator(-2)).to.include('error.nonpositive');
             });
         });
+
         context('for numbers divisible by 3:', function () {
             it('like 3, will include error.three', function () {
                 expect(validator(3)).to.include('error.three');
             });
+
             it('like 15, will include error.three', function () {
                 expect(validator(15)).to.include('error.three');
             });
         });
+
         context('for numbers divisible by 5:', function () {
             it('like 5, will include error.five', function () {
                 expect(validator(5)).to.include('error.five');
             });
+
             it('like 15, will include error.five', function () {
                 expect(validator(15)).to.include('error.five');
             });
